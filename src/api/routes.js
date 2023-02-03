769009547fb4cf4798366307dffba831c15d9cee
mhc <mhchau0+ada@gmail.com>
2023-02-02 20:42:06 -0500

    created updateDeck route

--- src/api/routes.js
+++ src/api/routes.js
@@ -28,6 +28,14 @@ async function createDeck(newtitle) {
   console.log(`New deck created!`);
 }
 // UPDATE a deck
+async function updateDeck(id, newtitle) {
+  const { data, error } = await supabase
+    .from("decks")
+    .update({ title: newtitle })
+    .eq("id", id);
+  console.log(`Deck ${id} successfully updated! New title is ${newtitle}`);
+}
+
 // DELETE a deck
 
 // Card functions
@@ -38,4 +46,4 @@ async function createDeck(newtitle) {
 // UPDATE a card
 // DELETE a card
 
-export { getAllDecks, getDeck, createDeck };
+export { getAllDecks, getDeck, createDeck, updateDeck };
